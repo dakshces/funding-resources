be30c60c11687e034f26cd900c1b0e2c8b4fe807
Jiyoung Chang <changjy01@gmail.com>
2021-05-13 19:27:05 +0000

    adminpage_labels

--- app/admin/resources.rb
+++ app/admin/resources.rb
@@ -24,78 +24,57 @@ ActiveAdmin.register Resource do
   permit_params :title, :description, :amount, :source, :contact_person, :web, :approved, :email, tag_ids: []
   
   # Customize the form for editing and adding resources
-  form do |f|
-    f.inputs do
-      f.input :title
-      f.input :description
-      f.input :amount
-      f.input :source
-      f.input :contact_person
-      f.input :email
-      f.input :web
-      f.input :tags, as: :check_boxes
-      f.input :approved
 
-      # there is a connection between resources and tags
-    end
-    f.actions
-    
-  end
-  
-    filter :contact_person, as: :select
-    filter :source, as: :select
-    filter :approved, as: :check_boxes
-    filter :title
-    filter :created_at
-    filter :updated_at
-    filter :description
-    filter :web
-    filter :email
-    filter :tags, as: :check_boxes
- 
-  config.sort_order = 'approved_desc'
-  
-  # Customize columns displayed on the index screen in the table
-  index do
-    column :approved
-    column :title
-    column :contact_person
-    column :email
-    column :created_at
-    column :updated_at
-    actions
-    # column "Approved", sortable: :approved do |resource|
-  end
-
-  # Customize columns displayed on the index screen in the table
-  index do
-    selectable_column
-    column :approved
-    column :title
-    column :contact_person
-    column :source
-    column :created_at
-    column :updated_at
-    actions
-  end
-
-  # Customize filters displayed on the left of the resources
-  filter :contact_person, as: :select
-  filter :source, as: :select
-  filter :approved, as: :check_boxes
-  filter :title
-  filter :created_at
-  filter :updated_at
-  filter :description
-  filter :web
-  filter :email
-  filter :tags, as: :check_boxes
- 
-  config.sort_order = 'approved_desc'
-
-end
 
 # Alex notes:
 # has_many_through attribute for tags and resources
 # register tags on active admin
 # register model (command line): $> rails generate active_admin:resource [MyModelName]
+
+# Customize the form for editing and adding resources
+  form do |f|
+    f.inputs do
+      f.input :title, label: 'Fund Name'
+      f.input :description, label: 'Description'
+      f.input :amount, label: 'Amount'
+      f.input :source, label: 'Department'
+      f.input :contact_person, label: 'Fund Owner'
+      f.input :email, label: 'Email'
+      f.input :web, label: 'Website'
+      f.input :tags, label: 'Tags', as: :check_boxes
+      f.input :approved, label: 'Approved'
+    end
+    f.actions
+  end
+
+ 
+
+
+# Customize columns displayed on the index screen in the table
+  index do
+    selectable_column
+    column 'Approved', :approved
+    column 'Fund Name', :title
+    column 'Fund Owner', :contact_person
+    column 'Department', :source
+    column :created_at
+    column :updated_at
+    actions
+  end
+ 
+
+  # Customize filters displayed on the left of the resources
+  filter :contact_person, label: 'Fund Owner', as: :select
+  filter :source, label: 'Department', as: :select
+  filter :approved, label: 'Approved', as: :check_boxes
+  filter :title, label: 'Fund Name'
+  filter :created_at
+  filter :updated_at
+  filter :description, label: 'Description'
+  filter :web, label: 'Website'
+  filter :email, label: 'Email'
+  filter :tags, label: 'Tags', as: :check_boxes
+ 
+  config.sort_order = 'approved_desc'
+  
+end
