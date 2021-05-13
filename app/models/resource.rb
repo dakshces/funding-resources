7c2ae593e90a22f2744e62c046f84cac91ea0c89
Ubuntu <ubuntu@ip-172-31-24-215.us-east-2.compute.internal>
2021-05-13 19:23:23 +0000

    Merged layout changes to main

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -12,14 +12,13 @@ class Resource < ApplicationRecord
   # Validates Resources Parameters
   validates :title, presence: true, uniqueness: { message: ": Fund Title has already been submitted." }, length: { maximum: 255 }
   
+  # Validates Resources Parameters for Add Fund page
   validates_with ContactValidator
   validates :contact_person, length: { maximum: 255 }
   validates :source, length: { maximum: 255 }
-  
-  # Validates Resources Parameters
   validates :title, presence: true, length: { maximum: 255 }
-  validates :contact_person, presence: true, length: { maximum: 255 }
-  #validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  # validates :contact_person, presence: true, length: { maximum: 255 }
+  # validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   
   # Not currently validating email  
   # This email regex could be improved
